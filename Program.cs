0f984e535983491e471ef6c22e0989ac1a64b9cf
Eric Taylor <ericfromspringfield@gmail.com>
2019-10-04 15:42:25 -0500

    Commit 1.0

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+List<int> numbers = new List<int> {
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+};
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
